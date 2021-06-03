984feec1eb94c3f1f2c8631fd7dddae133fbcf3d
itsirajul <shirajulislamparvez@gmail.com>
2021-06-04 03:07:08 +0600

    Gain little bit knowledge about datatypes

---
+++ src/01.Introduction/03.Variable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    int result;
+
+    a=5;
+    b=2;
+    a=a+1;
+    result=a-b;
+
+    std::cout<<result;
+
+    return 0;
+}
---
+++ src/01.Introduction/04.DataTypes.cpp
@@ -0,0 +1,20 @@
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+     int a;//this is called variable declaration 
+     a=12;// this is called initalisation.initialisation means assign to give some mark or identity on variable 
+
+        cout<<"size of int: "<<sizeof(a)<<endl;
+
+     float b;
+        cout<<"size of float: "<<sizeof(b)<<endl;
+
+    char c;
+         cout<<"size of character: "<<sizeof(c)<<endl;
+
+    bool d;
+         cout<<"size of boolean: "<<sizeof(d)<<endl;
+return 0;
+
+ }
