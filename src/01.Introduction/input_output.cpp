d976accc3aa41fc7f3951606db99824d24e469c0
itsirajul <shirajulislamparvez@gmail.com>
2021-06-06 21:44:40 +0600

    add explanational comment

---
+++ src/01.Introduction/input_output.cpp
@@ -0,0 +1,7 @@
+#include<iostream>//Preprocessor & HeaderFile
+using namespace std; 
+int main()//To Execution of code begins from main function(mandatory for anyprogram)
+{
+    std::cout<<"Hello World";//display output and ;marks the end of a statement 
+    return 0;//Exit Status of a function
+}//Curly Braces indicated indicate the start and the end of the function
