bc1fe16c1d0fe86495a614df86b321a59804c316
itsirajul <shirajulislamparvez@gmail.com>
2021-06-06 21:46:29 +0600

    adding line break

--- src/01.Introduction/input_output.cpp
+++ src/01.Introduction/input_output.cpp
@@ -2,6 +2,6 @@
 using namespace std; 
 int main()//To Execution of code begins from main function(mandatory for anyprogram)
 {
-    std::cout<<"Hello World";//display output and ;marks the end of a statement 
+    std::cout<<"Hello World\n";//display output and ;marks the end of a statement 
     return 0;//Exit Status of a function
 }//Curly Braces indicated indicate the start and the end of the function
