62af3d61695126e6e53d5033f7598e1c807d0caf
itsirajul <shirajulislamparvez@gmail.com>
2021-06-09 20:57:45 +0600

    small problem

--- src/05.Switch_Case_Statement/01.Switch01.cpp
+++ src/05.Switch_Case_Statement/01.Switch01.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
-using name
+using namespace std;
+int main()
+{
+     char button;
+     cout<<"Input a character"<<endl;
+     cin>>button;
+
+     if(button=='a')
+     {
+         cout<<"Hello"<<endl;
+     }
+     else if(button=='b')
+     {
+         cout<<"Namaste"<<endl;
+     }
+     else if(button=='c')
+     {
+         cout<<"Kaisha Ho !"<<endl;
+     }
+}
