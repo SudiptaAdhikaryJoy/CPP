44939f9571770f2c4007a9fad905afd02af06a12
itsirajul <shirajulislamparvez@gmail.com>
2021-06-10 06:38:01 +0600

    operator with mix (increment and decrement)

--- src/06.Operators/02.Decrement.cpp
+++ src/06.Operators/02.Decrement.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     int i=0;
-
-    i=i++ - --i + ++i - i--;
-
+       //0   //0   //1   //1
+    i= i++ - --i + ++i - i--;
+    //0
     cout<<i<<endl;
 
     return 0;
