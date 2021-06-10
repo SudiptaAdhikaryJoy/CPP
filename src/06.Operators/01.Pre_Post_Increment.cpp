8b5f81fa26415732a2646551af15857383bb65c2
itsirajul <shirajulislamparvez@gmail.com>
2021-06-10 06:34:34 +0600

    add little comment for explanation

--- src/06.Operators/01.Pre_Post_Increment.cpp
+++ src/06.Operators/01.Pre_Post_Increment.cpp
@@ -16,7 +16,8 @@ int main()
     int i=1;
     int j=2;
     int k;
-    k=i+j+i++ + j++ + ++i + ++j;
+     //1 //2 //1 //2    //3   //4   
+    k=i + j + i++ + j++ + ++i + ++j;
     cout<<i<<" "<<j<<" "<<" "<<k<<endl;
 
     return 0;
