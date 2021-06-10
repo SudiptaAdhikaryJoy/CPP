1fecc2497a770c81ab59fcf198d326c1d1eb242e
itsirajul <shirajulislamparvez@gmail.com>
2021-06-10 06:30:32 +0600

    pre post increment and decrement

--- src/06.Operators/01.Pre_Post_Increment.cpp
+++ src/06.Operators/01.Pre_Post_Increment.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int main()
+/*int main()
 {
     int a=10;
     int b;
 
     b=++a;
     cout<<a<<" "<<b<<endl;
+    return 0;
+}
+*/
+int main()
+{
+    int i=1;
+    int j=2;
+    int k;
+    k=i+j+i++ + j++ + ++i + ++j;
+    cout<<i<<" "<<j<<" "<<" "<<k<<endl;
+
     return 0;
 }
