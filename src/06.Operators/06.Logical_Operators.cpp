0ae8b8b8cd5a64984f071f1a381bff4e454f040a
itsirajul <shirajulislamparvez@gmail.com>
2021-06-11 23:19:46 +0600

    To Check Program Divisible by 2 &3 or not using operators

--- src/06.Operators/06.Logical_Operators.cpp
+++ src/06.Operators/06.Logical_Operators.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin>>n;
+
+    if(n%2==0 && n%3==0){
+        cout<<"Divisible by both"<<endl;
+    }
+    else if(n%2==0)
+    {
+        cout<<"Divisible by Only 2"<<endl;
+    }
+    else if(n%3==0)
+    {
+        cout<<"Divisible by only 3"<<endl;
+    }
+    else{
+        cout<<"Divisible by none"<<endl;
+    }
+    return 0;
 }
