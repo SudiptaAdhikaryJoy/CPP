fb5e12004917aa6c5b9375da36dcaa8884fe06ab
itsirajul <shirajulislamparvez@gmail.com>
2021-06-12 21:21:55 +0600

    Rectangle Pattern From Taking Input From User

--- src/07.Pattern_Question/01.Rectangle.cpp
+++ src/07.Pattern_Question/01.Rectangle.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 int main()
 {
-    
+    int row,col;
+    cin>>row>>col;
+
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=col;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
